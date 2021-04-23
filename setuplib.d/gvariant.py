6b516dfc483a9aeec6ddb06d7c4d21ecbd424d6e
MestreLion <linux@rodrigosilva.com>
2021-04-23 20:06:56 -0300

    GVariant quoting and typing the right way

--- setuplib.d/gvariant.py
+++ setuplib.d/gvariant.py
@@ -5,60 +5,151 @@
 
 __all__ = [
     'array',
+    'quote',
+    'unquote',
 ]
 
 import ast
 import os.path
 import sys
 
-TYPES = ('s', 'u')
+try:
+    from gi.repository import GLib
+except ImportError:
+    GLib = None
+
+
+STRINGS  = ('s', 'o', 'g')
+INTEGERS = ('y', 'n', 'q', 'i', 'u', 'x', 't', 'h')
+BASIC_TYPES = {
+    'd': float,
+    'b': lambda x: x == 'true'
+}
+BASIC_TYPES.update({_: str for _ in STRINGS})
+BASIC_TYPES.update({_: int for _ in INTEGERS})
 
 
 # Exported methods, must have (*args) signature -------------------------------
+
+def quote(*args):
+    """Print the GVariant textual representation of a string"""
+    if not len(args) == 1:
+        return usage(f"Missing or extra arguments: {args}", "STRING")
+    print(repr(args[0]))  # print(gvariant_repr(args[0], 's'))
+
+
+def unquote(*args):
+    """Print a string given its GVariant textual representation"""
+    if not len(args) == 1:
+        return usage(f"Missing or extra arguments: {args}", "STRING")
+    print(parse_repr(args[0], str))  # print(parse_gvariant(args[0], 's'))
 
 
 def array(*args):
     if len(args) < 3:
         return usage(f"Missing required arguments in {args}",
                      "OPERATION ITEM_TYPE LIST_STRING [ITEMS...]")
-    itemtype, newlist = _array(*args)
-    print(f'@a{itemtype} {newlist!r}')
+    newlist = _array(*args)
+    # strictly speaking, GVariant only includes type annotation for empty arrays,
+    # but since we're not thoroughly testing each item, add it as safety measure
+    print(f'@a{args[1]} {newlist!r}')  # print(gvariant_repr(newlist, 'a' + args[1]))
 
 
-def _array(op: str, itemtype: str, strlist: str, *items) -> 'Tuple[str, list]':
-    if itemtype not in TYPES:
-        raise GVariantError(f"Not a supported item type {TYPES}: {itemtype}")
+def _array(op: str, itemtype: str, strlist: str, *items) -> list:
+    if itemtype not in BASIC_TYPES:
+        raise GVariantError("Not a supported item type"
+                            f" {BASIC_TYPES.keys()}: {itemtype!r}")
 
-    if itemtype == 'u':
-        items = list(map(int, items))
+    # Shortcut for clear/new, as it ignores items
+    # (maybe should raise exception if there are items?)
+    if op in ('clear', 'new'):
+        return []
 
-    # Shortcut for 'set' and 'clear', as they don't require handling strlist
+    items = list(map(BASIC_TYPES[itemtype], items))
+
+    # Shortcut for 'set', as it doesn't handle strlist
     if op == 'set':
-        return itemtype, list(items)
-    elif op in ('clear', 'new'):
-        return itemtype, []
+        return items
 
-    # type annotation for empty lists: '@as []'
+    # Type annotation. For empty arrays of strings, '@as []'
     if strlist.startswith('@'):
-        strlist = strlist.split(' ', 1)[1]
-        # TODO: maybe set itemtype too?
+        vartype, strlist = strlist.split(' ', 1)
+        if vartype != typeanno:
+            raise GVariantError(f"Array type mismatch: LIST_STRING has {vartype!r},"
+                                f" but ITEM_TYPE implies {annotation!r}")
 
-    try:
-        curlist = ast.literal_eval(strlist)
-    except (SyntaxError, ValueError):
-        raise GVariantError(f"Malformed list literal: {strlist!r}")
-    if not isinstance(curlist, list):
-        raise GVariantError(f"Not a list literal: {strlist!r}")
+    curlist = parse_repr(strlist, list)  # parse_gvariant(strlist, 'a'+itemtype)
 
     # Use list instead of set to preserve order
     if op == 'remove':
-        newlist = [_ for _ in curlist if _ not in items]
+        return [_ for _ in curlist if _ not in items]
     elif op == 'include':
-        newlist = curlist + [_ for _ in items if _ not in curlist]
-    else:
-        raise GVariantError(f"Not a valid list operation: {op!r}")
+        return curlist + [_ for _ in items if _ not in curlist]
 
-    return itemtype, newlist
+    raise GVariantError(f"Not a valid list operation: {op!r}")
+
+
+# Supporting functions ---------------------------------------------------------
+
+def parse_repr(text, cls=None):
+    """Return a Python builtin object from a literal expression string
+
+    Useful for reverting repr(obj) if obj is a Python builtin, as the textual
+    representation of all builtins, as given by their repr(), is always a literal
+    expression evaluating back to themselves. Hence the function name.
+    """
+    try:
+        obj = ast.literal_eval(text)
+    except (SyntaxError, ValueError):
+        raise GVariantError(f"Malformed literal: {text!r}")
+    if cls and not isinstance(obj, cls):
+        raise GVariantError(f"Not a {cls.__name__} literal: {text!r}")
+    return obj
+
+
+def parse_gvariant(text: str, vartype: str or None = None):
+    """Return a Python builtin object from a GVariant textual representation
+
+    To revert, in the general case vartype is reused and must not be None:
+    text == str(GLib.Variant(vartype, parse_gvariant(text, vartype)))
+        In some cases can be reverted simply by: text == repr(parse_gvariant(text))
+
+    GVariant mini-reference:
+
+    GLib.Variant(type: str, value: object) -> GLib.Variant
+        - type must not be falsy and is not inferred from object
+        - Not all objects are valid values. Most if not all Python builtins are.
+          Heterogeneous lists are not.
+
+    GLib.Variant.parse(type: VariantType or None, text: str) -> GLib.Variant
+        - If type is None it infers from text, in which case if text represents
+          an empty array it must be prefixed with type annotation: '@as []'
+
+    str(GLib.Variant.parse(None, repr(obj))) == repr(obj)
+        - Unless obj is an empty list, when its str() prepends the type annotation
+          (and in this case can't pass None as type anyway)
+        - Assuming a valid obj and also a repr(obj) that evaluates as itself.
+
+    str(gvariant) == gvariant.print_(True)
+        - Despite the docs, only actually include type annotation for empty arrays
+    """
+    if not GLib:
+        raise GVariantError(f"Support for parsing GVariants"
+                            " requires Python GLib bindings")
+    if vartype:
+        vartype = GLib.VariantType.new(vartype)
+    else:
+        vartype = None  # covers the case when vartype is empty string
+
+    return parse_repr(GLib.Variant.parse(vartype, text).print_(False))
+
+
+def gvariant_repr(obj: object, vartype: str):
+    """Return the GVariant textual representation of an object"""
+    if not GLib:
+        raise GVariantError(f"Support for parsing GVariants"
+                            " requires Python GLib bindings")
+    return str(GLib.Variant(vartype, obj))
 
 
 # Dispatching engine -----------------------------------------------------------
